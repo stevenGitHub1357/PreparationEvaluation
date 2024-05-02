7ba79fbfbbf91ee444d97524bc6a8f423742298a
stevenGitHub1357 <stevenratovoson@gmail.com>
2024-05-02 15:40:55 +0300

    Projet Vierge

--- ProjetEval/Controllers/HomeController.cs
+++ ProjetEval/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PreparationFinal.Models;
+using ProjetEval.Models;
 
-namespace PreparationFinal.Controllers;
+namespace ProjetEval.Controllers;
 
 public class HomeController : Controller
 {
