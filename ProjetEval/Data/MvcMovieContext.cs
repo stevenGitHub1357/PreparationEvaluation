5292cbd20d4dd460c9d4b7726165e2d540a059b0
stevenGitHub1357 <stevenratovoson@gmail.com>
2024-05-11 08:55:58 +0300

    Generator Controler View from Model

---
+++ ProjetEval/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetEval.Models;
+
+namespace Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- ProjetEval/Program.cs
+++ ProjetEval/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
